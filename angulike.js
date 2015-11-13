3187f5e04029557728eef1da9ff7ddfe45ac144b
Stewan P <talk@stpa.co>
2015-11-13 19:43:14 -0200

    update(lib): version and appId setting

--- angulike.js
+++ angulike.js
@@ -15,9 +15,9 @@
                     // Load Facebook SDK if not already loaded
                     $.getScript('//connect.facebook.net/pt_BR/sdk.js', function() {
                         $window.FB.init({
-                            appId: setting.facebook.appId, //@todo make me an attribute
+                            appId: setting.store.facebook.appId, //@todo make me an attribute
                             xfbml: true,
-                            version: 'v2.0'
+                            version: 'v2.3'
                         });
                         renderLikeButton();
                     });
